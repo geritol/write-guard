bfd13b7689a63e04163b1cef7c18577b1461b3aa
Gergo Tolnai <gergo.tolnai@gmail.com>
2020-07-04 15:04:49 +0200

    fix: exit 1 in case of any errors

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 const writeGuard = require("./write-guard");
 const loadSettings = require("./load-settings");
 
+const error = (message) => {};
+
 (async () => {
   const settings = loadSettings(".");
-  const accessInfo = await writeGuard(settings);
+  try {
+    var accessInfo = await writeGuard(settings);
+  } catch (error) {
+    error(error.message);
+  }
   if (accessInfo.some((info) => !info.canWrite)) {
-    console.error(
+    error(
       `User '${settings.github.user}' has no write access to all files in pull request #${settings.github.prNumber}`
     );
-    process.exit(1);
   }
 })();
